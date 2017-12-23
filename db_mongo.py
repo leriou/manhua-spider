c1cf5a649d8b1f8d5972c577fdfdf54b32e4be1a
leriou <yange1876@hotmail.com>
2017-12-23 18:28:49 +0800

    commit mongo adapter

--- db_mongo.py
+++ db_mongo.py
@@ -3,8 +3,16 @@
 
 class Db:
 
-    def getConn(self,host):
-        pass
+    def getConn(self,conf):
+        self.host = conf['host'] 
+        self.port = conf['port']
+        self.username = conf['username']
+
+    def getClient(self)
+        if Db.Client:
+            return Db.Client
+        else:
+            return False
         
     # 插入数据
     def insert(self,data):
@@ -12,3 +20,6 @@ class Db:
     # 更新数据
     def update(self,data):
         pass
+
+    def delete(self,data):
+        self.update()
